9657a585389bfacf87767f74b388e878e4733ef6
Charles Heseltine <charlieheseltine@Charless-MacBook-Pro.local>
2023-06-29 10:43:58 +0100

    Adding Host T7.2)

--- db/connection.js
+++ db/connection.js
@@ -7,8 +7,15 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.PGDATABASE_URL) {
+  throw new Error('PGDATABASE or URL not set');
 }
 
-module.exports = new Pool();
+const config = {}
+
+if (ENV === 'production'){
+  config.connectionString = process.env.PGDATABASE_URL
+  config.max = 2
+}
+
+module.exports = new Pool(config);
