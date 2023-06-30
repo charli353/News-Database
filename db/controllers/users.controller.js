ebcc59ef64aeede7799874ca0fdd7a6126a8cb92
Charles Heseltine <charlieheseltine@Charless-MacBook-Pro.local>
2023-06-30 11:35:22 +0100

    T10.final

--- db/controllers/users.controller.js
+++ db/controllers/users.controller.js
@@ -3,8 +3,9 @@ const { retrieveUsers } = require(`../models/users.model`)
 function getAllUsers(req, res, next) {
     retrieveUsers().then((users) => {
         console.log(users)
-        res.status(200).send({ 'users' : users })
+        res.status(200).send({users})
     })
+    .catch(next)
 }
 
 module.exports = { getAllUsers }
